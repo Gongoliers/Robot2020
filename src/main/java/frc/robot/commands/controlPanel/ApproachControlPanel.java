7b18099383f103405f99d6f928651c0ed677b79c
Richard Buckley <42151145+rhbuckley@users.noreply.github.com>
2020-02-02 14:02:54 -0500

    Formatting changes to ApproachControlPanel

--- src/main/java/frc/robot/commands/controlPanel/ApproachControlPanel.java
+++ src/main/java/frc/robot/commands/controlPanel/ApproachControlPanel.java
@@ -4,44 +4,44 @@ import edu.wpi.first.wpilibj.command.Command;
 import frc.robot.Robot;
 
 public class ApproachControlPanel extends Command {
-  int DISTANCE_FROM_SENSOR_TO_PANEL = 1000; // TODO: Tune this value
-  double DRIVING_FORWARD_SPEED = .2; // TODO: Tune this value
-  public ApproachControlPanel() {
-    requires(Robot.drivetrain);
-  }
+    private static final int DISTANCE_FROM_SENSOR_TO_PANEL = 1000; // TODO: Tune this value
+    private static final double DRIVING_FORWARD_SPEED = .2; // TODO: Tune this value
 
-  // Called just before this Command runs the first time
-  @Override
-  protected void initialize() {
-  }
-
-  // Called repeatedly when this Command is scheduled to run
-  @Override
-  protected void execute() {
-    Robot.drivetrain.arcadeDrive(DRIVING_FORWARD_SPEED, 0);
-  }
-
-  // Make this return true when this Command no longer needs to run execute()
-  @Override
-  protected boolean isFinished() {
-    if (Robot.controlPanelManipulator.getProximity() >= DISTANCE_FROM_SENSOR_TO_PANEL) {
-      return true;
+    public ApproachControlPanel() {
+        requires(Robot.drivetrain);
     }
-    else{
-      return false;
+
+    // Called just before this Command runs the first time
+    @Override
+    protected void initialize() {
     }
-  }
 
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-    Robot.drivetrain.stop();
-  }
+    // Called repeatedly when this Command is scheduled to run
+    @Override
+    protected void execute() {
+        Robot.drivetrain.arcadeDrive(DRIVING_FORWARD_SPEED, 0);
+    }
 
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
-    end();
-  }
+    // Make this return true when this Command no longer needs to run execute()
+    @Override
+    protected boolean isFinished() {
+        if (Robot.controlPanelManipulator.getProximity() >= DISTANCE_FROM_SENSOR_TO_PANEL) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    // Called once after isFinished returns true
+    @Override
+    protected void end() {
+        Robot.drivetrain.stop();
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    @Override
+    protected void interrupted() {
+        end();
+    }
 }
